003936f4c889e49609f0ae7c5df748aa8bbb9735
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-01-13 20:12:34 +0530

    Create 1.cpp

---
+++ 001 - 500/1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> numMap;
+        int n = nums.size();
+
+        for(int i=0;i<n;i++){
+            int rem = target - nums[i];
+            if(numMap.count(rem)){
+                return {numMap[rem], i};
+            }
+            numMap[nums[i]] = i;
+        }
+     return {};
+    }
+};
