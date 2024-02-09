6d6aa61b11ae4dea4c70f24fe08fd5eb862f0763
Puneethkumar <puneethkumarg96@gmail.com>
2024-02-09 12:44:13 +0530

    refactor

--- 001 - 500/011.cpp
+++ 001 - 500/011.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int i = 0,j = height.size()-1;
+        int left = 0;
+        int right = height.size()-1;
         int maxArea = 0;
-        while(i < j){
-            int currentArea = min(height[i],height[j])*(j-i);
-            maxArea = max(maxArea,currentArea);
-            if(height[i] < height[j]){
-                i++;
-            }
-            else{
-                j--;
+        while(left < right){
+            int currArea = min(height[left],height[right]) * (right - left);
+            maxArea = max(maxArea,currArea);
+
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
             }
         }
         return maxArea;
