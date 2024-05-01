6d95b2d557db2ed29b551e3a4f99d4fa4c5519a6
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-05-02 04:44:14 +0530

    Create 1544.cpp

---
+++ 1501-2000/1544.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+
+        for(char i:s){
+            if(st.empty())st.push(i);
+            else if(abs(st.top() - i) == 32 )st.pop();
+            else st.push(i);
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans = st.top()+ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
