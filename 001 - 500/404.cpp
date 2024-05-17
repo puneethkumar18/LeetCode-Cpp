6603a93682bbca86342ec957ee8c4f31b45cacbe
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-05-17 06:57:18 +0530

    Create 404.cpp

---
+++ 001 - 500/404.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void solve(TreeNode* root,int &sum){
+        if(root == NULL){
+            return ;
+        }
+        solve(root->left,sum);
+        if(root->left && !root->left->left && !root->left->right){
+            sum = sum + root->left->val;
+        }
+        solve(root->right,sum);
+       
+    }
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        solve(root,sum);
+        return sum;
+    }
+};
