f6165a1781ae3e84332a61234dbe4e44d3cb9b50
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-08-11 16:39:21 +0530

    Create 3146.cpp

---
+++ 3146.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        int ans= 0;
+        for(int i=0;i<s.size();i++){
+            for(int j=0;j<s.size();j++){
+                if(s[i] == t[j]){
+                    ans+= abs(i-j);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
