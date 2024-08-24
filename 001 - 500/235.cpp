ebab422a8338735f79bc9c1d672310cf01f9c899
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-08-24 16:38:44 +0530

    Create 235.cpp

---
+++ 001 - 500/235.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root == NULL){
+            return NULL;
+        }
+
+        if(p->val < root->val && q->val < root->val){
+            return solve(root->left,p,q);
+        }
+        else if(p->val > root->val && q->val > root->val)
+        {
+            return solve(root->right,p,q);
+        }
+        return root;
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return solve(root,p,q);
+    }   
+};
