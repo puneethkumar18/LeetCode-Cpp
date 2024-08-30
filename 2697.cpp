4c16b657a7738664c23bea5aa4a12e4e9dd98dca
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-08-30 09:21:44 +0530

    Create 2697.cpp

---
+++ 2697.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s[i] != s[j]) {
+                if ((int)s[i] < (int)s[j]) {
+                    s[j] = s[i];
+                }
+
+                else {
+                    s[i] = s[j];
+                }
+            }
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
