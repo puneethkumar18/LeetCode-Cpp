a7ff02acb5a046a643ae20b53dc3cb8634034481
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-09-03 21:33:13 +0530

    Create 078.cpp

---
+++ 001 - 500/078.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void solve(vector<int>& nums,vector<vector<int>> &ans,int index,vector<int> temp){
+        if(index == nums.size()){
+            ans.push_back(temp);
+            return ;
+        }
+        solve(nums,ans,index+1,temp);
+        temp.push_back(nums[index]);
+        solve(nums,ans,index+1,temp);
+
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(nums,ans,0,temp);
+
+        return ans;
+    }
+};
