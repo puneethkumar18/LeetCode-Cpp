5c8189b306f01a7a2c2cb159b99b2ac0a5602277
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-09-03 20:47:51 +0530

    Create 2176.cpp

---
+++ 2176.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        int ans = 0;
+        if(n < 2){
+            return ans;
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if((i*j)%k  == 0 && nums[i] == nums[j]){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
