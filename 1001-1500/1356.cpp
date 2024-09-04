0cfa61837176b2ac05230e46a7ee706c77d9185b
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-09-04 20:36:38 +0530

    Update 1356.cpp

--- 1001-1500/1356.cpp
+++ 1001-1500/1356.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    int getDeciCount(int num){
+        int count = 0;
+        while(num > 0){
+            if(num%2 != 0){
+                count++;
+            }
+            num = num/2;
+        }
+        return count;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        map<int,vector<int>> m;
+        for(int i=0;i<arr.size();i++){
+            int temp = getDeciCount(arr[i]);
+            m[temp].push_back(arr[i]);
+        }
 
+        vector<int> ans;
+        for(auto i:m){
+            sort(i.second.begin(),i.second.end());
+            for(auto j:i.second){
+                ans.push_back(j);
+            }
+        }
+        return ans;
+    }
+};
