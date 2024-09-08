ff63e356a760c07b7d5ee65a129d45dbb138877e
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-09-08 16:03:03 +0530

    Create 124.cpp

---
+++ 001 - 500/124.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int solve(TreeNode* root,int& ans){
+        if(root == NULL){
+            return 0;
+        }
+        int left = solve(root->left,ans);
+        int right = solve(root->right,ans);
+
+        int sum = left+right+root->val;
+
+        if(sum > ans){
+            ans = sum;
+        }
+        int mini = min(left,right);
+        if( sum-mini < 0){
+            return 0;
+        }
+        return sum-mini;
+    }
+    int maxPathSum(TreeNode* root) {
+        int ans = INT_MIN;
+        solve(root,ans);
+        return ans;
+    }
+};
