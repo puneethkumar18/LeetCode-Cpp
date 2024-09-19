3254e4f8501ec1695c7aa235d0f7cd4dc679ca84
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2024-09-19 22:13:24 +0530

    Create 1577.cpp

---
+++ 1501-2000/1577.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int> ans;
+        unordered_map<int, int> m;
+
+        for(int i=0;i<edges.size();i++){
+            m[edges[i][1]]++;
+        }
+
+        for(int i=0;i<n;i++){
+            if(m[i] == 0){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
