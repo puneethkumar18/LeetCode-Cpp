e02587dcadf752818ced29094ac15761ca106605
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2025-01-02 21:40:09 +0530

    Update 2006.cpp

--- 2000-3500/2006.cpp
+++ 2000-3500/2006.cpp
@@ -4,7 +4,6 @@ public:
         int ans = 0;
         for(int i=0;i<nums.size();i++){
             for (int j=0;j<nums.size();j++){
-
                 
                 if((nums[i] - nums[j]) == k){
                     ans++;
