1c97711042adced0f3eae7080689370e1e6603ff
PUNEETH KUMAR <119596819+puneethkumar18@users.noreply.github.com>
2025-02-04 20:04:29 +0530

    Create Floyd'sWarshallAlgo.cpp

---
+++ Graph/Floyd'sWarshallAlgo.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    void shortestDistance(vector<vector<int>>& mat) {
+        // Code here
+        
+        int n = mat.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == -1){
+                    mat[i][j] = 1e9;
+                }
+                if(i == j)mat[i][j] = 0;
+            }
+        }
+        for(int via=0;via<n;via++){
+            for(int i=0;i<n;i++){
+                //if(j == i) continue;
+                for(int j=0;j<n;j++){
+                    
+                    mat[i][j] = min(mat[i][via]+mat[via][j],mat[i][j]);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == 1e9){
+                    mat[i][j] = -1;
+                }
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> matrix(n, vector<int>(n, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        Solution obj;
+        obj.shortestDistance(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
